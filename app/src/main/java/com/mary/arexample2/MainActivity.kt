1e1c216415b6cbc65e648a7d6241bb9ff737f277
Mary <littlemary1379@gmail.com>
2021-03-03 14:47:53 +0900

    create Session(incomplete exception)

--- app/src/main/java/com/mary/arexample2/MainActivity.kt
+++ app/src/main/java/com/mary/arexample2/MainActivity.kt
@@ -3,9 +3,16 @@ package com.mary.arexample2
 import android.Manifest
 import android.content.pm.PackageManager
 import android.os.Bundle
+import android.view.GestureDetector
+import android.view.MotionEvent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
+import com.google.ar.core.ArCoreApk
+import com.google.ar.core.Config
+import com.google.ar.core.Session
+import com.google.ar.core.exceptions.*
 import com.google.ar.sceneform.ArSceneView
+import com.google.ar.sceneform.Scene
 import com.mary.arexample2.util.DlogUtil
 import com.mary.arexample2.util.PermissionCheckUtil
 
@@ -18,16 +25,39 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var arSceneView: ArSceneView
 
+    private var session: Session? = null
+    private var installRequest: Boolean = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         permissionCheck()
         findView()
+        checkARcore()
+        setListener()
     }
 
     private fun findView() {
         arSceneView = findViewById(R.id.arSceneView)
+    }
+
+    private fun setListener() {
+        var gestureDetector = GestureDetector(this, object : GestureDetector.SimpleOnGestureListener() {
+            override fun onSingleTapUp(e: MotionEvent?): Boolean {
+                onSingleTapUp(e)
+                return true
+            }
+
+            override fun onDown(e: MotionEvent?): Boolean {
+                return true
+            }
+        })
+
+        arSceneView.scene.setOnTouchListener(Scene.OnTouchListener { hitTestResult, motionEvent ->
+            return@OnTouchListener gestureDetector.onTouchEvent(motionEvent)
+        })
+
     }
 
     //1. permission
@@ -38,11 +68,12 @@ class MainActivity : AppCompatActivity() {
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
 
-        var permissionSize : Int = permissions.size
+        var permissionSize: Int = permissions.size
 
-        for(i : Int in 0 until permissionSize) {
-            if(ContextCompat.checkSelfPermission(this,permissions[i]) == PackageManager.PERMISSION_DENIED) {
+        for (i: Int in 0 until permissionSize) {
+            if (ContextCompat.checkSelfPermission(this, permissions[i]) == PackageManager.PERMISSION_DENIED) {
                 DlogUtil.d(TAG, "권한 미승인")
+                finish()
             } else {
                 DlogUtil.d(TAG, "${permissions[i]} 권한 승인")
             }
@@ -51,7 +82,47 @@ class MainActivity : AppCompatActivity() {
     }
 
     //2. Create Session
+    //세션을 만들기 전 AR core이 지원되는지 아닌지 확인하고 세션을 생성한다.
+    private fun checkARcore() {
+        try {
+            if (session == null) {
+                when (ArCoreApk.getInstance().requestInstall(this, installRequest)) {
+                    ArCoreApk.InstallStatus.INSTALL_REQUESTED -> {
+                        DlogUtil.d(TAG, "AR core 설치 필요")
+                        installRequest = true
+                    }
+                    ArCoreApk.InstallStatus.INSTALLED -> {
+                        DlogUtil.d(TAG, "AR core 설치 미필요")
+                        createSession()
+                    }
+                }
+            }
+        } catch (e: UnavailableArcoreNotInstalledException) {
+            DlogUtil.d(TAG, "ARCore 설치 필요")
+        } catch (e: UnavailableUserDeclinedInstallationException) {
+            DlogUtil.d(TAG, "ARCore 설치 필요")
+        } catch (e: UnavailableApkTooOldException) {
+            DlogUtil.d(TAG, "ARCore 업데이트 필요")
+        } catch (e: UnavailableSdkTooOldException) {
+            DlogUtil.d(TAG, "앱 업데이트 필요")
+        } catch (e: UnavailableDeviceNotCompatibleException) {
+            DlogUtil.d(TAG, "디바이스가 AR core을 지원하지 않음")
+        } catch (e: Exception) {
+            DlogUtil.d(TAG, "AR 세션 생성 실패")
+        }
+    }
 
+    private fun createSession() {
+        DlogUtil.d(TAG, "세션 생성")
+        session = Session(this)
+        val config = Config(session)
+        config.updateMode = Config.UpdateMode.LATEST_CAMERA_IMAGE
+
+        session?.configure(config)
+        arSceneView.setupSession(session!!)
+        arSceneView.resume()
+
+    }
 
 
 }
--- app/src/main/java/com/mary/arexample2/util/PermissionCheckUtil.kt
+++ app/src/main/java/com/mary/arexample2/util/PermissionCheckUtil.kt
@@ -13,23 +13,26 @@ object PermissionCheckUtil {
 
     var CAMERA_REQUEST_CODE = 10
 
-    fun checkPermission(activity: Activity, permission : Array<String>) {
+    fun checkPermission(activity: Activity, permission: Array<String>) {
 
-        var requestList : MutableList<String> = mutableListOf()
+        var requestList: MutableList<String> = mutableListOf()
 
-        for(i : Int in permission.indices){
+        for (i: Int in permission.indices) {
             var permissionCheck = ContextCompat.checkSelfPermission(activity, permission[i])
 
-            if(permissionCheck == PackageManager.PERMISSION_DENIED) {
+            if (permissionCheck == PackageManager.PERMISSION_DENIED) {
 
-                DlogUtil.d(TAG,  "권한 승인 필요")
+                DlogUtil.d(TAG, "권한 승인 필요")
                 requestList.add(permission[i])
 
+            } else {
+                DlogUtil.d(TAG, "${permission[i]} 이미 승인되어 있음.")
             }
 
         }
 
-        ActivityCompat.requestPermissions(activity, requestList.toTypedArray(), CAMERA_REQUEST_CODE)
-
+        if (!requestList.isNullOrEmpty()) {
+            ActivityCompat.requestPermissions(activity, requestList.toTypedArray(), CAMERA_REQUEST_CODE)
+        }
     }
 }
